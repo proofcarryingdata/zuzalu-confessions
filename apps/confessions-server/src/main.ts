7c370bb67d1964b9d511977c4f547d13b10d2998
Ivan Chub <ivansergchub@gmail.com>
2023-04-07 20:11:45 +0700

    another try

--- apps/confessions-server/src/main.ts
+++ apps/confessions-server/src/main.ts
@@ -1,18 +1,19 @@
+// prettier-ignore
 import * as dotenv from "dotenv";
 import * as fs from "fs";
 import * as path from "path";
-import { startApplication } from "./application";
 import { IS_PROD } from "./util/isProd";
 
 const dotEnvPath = IS_PROD
   ? `/etc/secrets/.env`
   : path.join(process.cwd(), ".env");
 
-console.log("env vars", fs.readFileSync(dotEnvPath).toString());
-
 console.log(`[INIT] Loading environment variables from: ${dotEnvPath} `);
 dotenv.config({ path: dotEnvPath });
 console.log("env vars", fs.readFileSync(dotEnvPath).toString());
+console.log("SEMAPHORE_GROUP_URL=", process.env.SEMAPHORE_GROUP_URL);
 
 console.log("[INIT] Starting application");
+
+import { startApplication } from "./application";
 startApplication();
