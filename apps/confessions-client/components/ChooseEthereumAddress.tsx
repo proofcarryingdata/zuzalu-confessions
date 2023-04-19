c5f0dc76b19baaacd71280491ea6cd1f04747d56
Ivan Chub <ivansergchub@gmail.com>
2023-04-19 15:52:10 +0200

    able to retrieve semaphore signature

--- apps/confessions-client/components/ChooseEthereumAddress.tsx
+++ apps/confessions-client/components/ChooseEthereumAddress.tsx
@@ -1,13 +1,34 @@
 import {
   constructPassportPcdGetRequestUrl,
   getWithoutProvingUrl,
+  usePassportPopupMessages,
 } from "@pcd/passport-interface";
-import { SemaphoreSignaturePCDPackage } from "@pcd/semaphore-signature-pcd";
+import {
+  SemaphoreSignaturePCD,
+  SemaphoreSignaturePCDPackage,
+} from "@pcd/semaphore-signature-pcd";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 import { PASSPORT_URL } from "../src/util";
 
 console.log(constructPassportPcdGetRequestUrl, getWithoutProvingUrl);
 
 export function ChooseEthereumAddress() {
+  const [pcdStr, _passportPendingPCDStr] = usePassportPopupMessages();
+  const [pcd, setPcd] = useState<SemaphoreSignaturePCD | undefined>();
+
+  useEffect(() => {
+    if (pcdStr !== "") {
+      SemaphoreSignaturePCDPackage.deserialize(JSON.parse(pcdStr).pcd).then(
+        setPcd
+      );
+    }
+  }, [pcdStr]);
+
+  useEffect(() => {
+    console.log(pcd);
+  }, [pcd]);
+
   return (
     <>
       <button
@@ -17,6 +38,7 @@ export function ChooseEthereumAddress() {
       >
         post as ethereum address
       </button>
+      {pcd && <SelectedIdentity>{pcd?.id?.substring(0, 16)}</SelectedIdentity>}
     </>
   );
 }
@@ -34,3 +56,8 @@ export function sendPassportRequest(proofUrl: string) {
   const popupUrl = `/popup?proofUrl=${encodeURIComponent(proofUrl)}`;
   window.open(popupUrl, "_blank", "width=360,height=480,top=100,popup");
 }
+
+const SelectedIdentity = styled.span`
+  border: 1px solid green;
+  padding: 1px 4px;
+`;
