1b51074fb70a2b7e4aacb026fab0e68361287b1f
Ivan Chub <ivansergchub@gmail.com>
2023-04-19 15:33:01 +0200

    fix some react warnings

--- apps/confessions-client/components/ChooseEthereumAddress.tsx
+++ apps/confessions-client/components/ChooseEthereumAddress.tsx
@@ -1,13 +1,42 @@
+import {
+  constructPassportPcdGetRequestUrl,
+  constructPassportPcdGetWithoutProvingRequestUrl,
+} from "@pcd/passport-interface";
+
 export function ChooseEthereumAddress() {
   return (
     <>
       <button
         onClick={() => {
-          alert("choosing ethereum address");
+          getProofWithoutProving();
         }}
       >
         post as ethereum address
       </button>
     </>
   );
+}
+
+function getProofWithoutProving() {
+  console.log(
+    "constructPassportPcdGetWithoutProvingRequestUrl",
+    constructPassportPcdGetWithoutProvingRequestUrl
+  );
+  console.log(
+    "constructPassportPcdGetRequestUrl",
+    constructPassportPcdGetRequestUrl
+  );
+
+  // const url = constructPassportPcdGetWithoutProvingRequestUrl(
+  //   PASSPORT_URL,
+  //   window.location.origin + "/popup",
+  //   EthereumOwnershipPCDPackage.name
+  // );
+
+  // sendPassportRequest(url);
+}
+
+export function sendPassportRequest(proofUrl: string) {
+  const popupUrl = `/popup?proofUrl=${encodeURIComponent(proofUrl)}`;
+  window.open(popupUrl, "_blank", "width=360,height=480,top=100,popup");
 }
